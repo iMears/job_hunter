4e312fbe33a37bb109e30adb918779098d77f254
iMears <maxwellmears@gmail.com>
2015-03-10 13:35:06 -0700

    added create and read for jobs page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,21 @@
 class JobsController < ApplicationController
   def index
+    @jobs = current_user.jobs
+  end
 
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.create(job_params)
+    current_user.jobs << @job
+    redirect_to user_jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:company, :position, :location, :job_email, :category)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310051512) do
+ActiveRecord::Schema.define(version: 20150310195016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150310051512) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "category",   default: "interested"
+    t.integer  "user_id"
   end
 
   create_table "notes", force: true do |t|
