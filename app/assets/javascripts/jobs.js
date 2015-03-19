31fff51c4639f249e1c67f8ccdffec00aac0b7cf
iMears <maxwellmears@gmail.com>
2015-03-19 13:19:50 -0700

    Added styling for job info on job show page, working on ajax for add note still

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,28 +1,28 @@
 $(document).ready (function(){
-  $(".save-note-btn").on('click', function(event) {
-    event.preventDefault();
-    if (event.target.text === "Save") {
-      addNote(event);
-    }
-  });
+  $(document).on('ajax:success', '.note-form', function(e, data, status, xhr) {
+    // console.log(status);
+    // console.log(xhr);
+    // console.log(e);
+    var template = "";
+    template += "<a href='/users/" + 6 + "/jobs/" + 33 + "/notes/" + 87 + "'>";
+    template += "<div class='note-container'>";
+    template += "<div class='row'>";
+    template += "<div class='pull-left'>";
+    template += "<h4 class='single-note-title'>";
+    template += "<em>FUCK STICK</em>";
+    template += "</h4>";
+    template += "</div>";
+    template += "<div class='pull-right note'>11/13/3333</div>";
+    template += "<div>this is how we do is bitches</div>";
+    template += "</div>";
+    template += "</a>";
 
-  function addNote(event) {
-    event.preventDefault();
-    console.log(event);
-    var $action = $(event.target.attributes.nodeValue);
-    var $data   = $(this).serialize();
-    var request = $.ajax({
-      url: $action,
-      type: 'post',
-      data: $data,
-      dataType: 'json'
-    });
-    request.done(function(response) {
-      console.log("got a request");
-    });
-    request.fail(function(xhr, status, error) {
-      var err = eval("(" + xhr.responseText + ")");
-      console.log(err);
-    });
-  }
+    $('.all-notes').append(template);
+  });
+  $(document).on('ajax:error', '.note-form', function(xhr, status, error) {
+    console.log("Error!!");
+    console.log(xhr);
+    console.log(status);
+    console.log(error);
+  });
 });
