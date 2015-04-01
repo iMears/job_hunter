81996ff751752f34201f6c9089e80f0b80324be4
imears <maxwellmears@gmail.com>
2015-03-31 23:19:21 -0700

    Added link to jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326221351) do
+ActiveRecord::Schema.define(version: 20150401061754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150326221351) do
     t.string   "phone_number"
     t.string   "manager"
     t.text     "description"
+    t.string   "link"
   end
 
   create_table "notes", force: true do |t|
